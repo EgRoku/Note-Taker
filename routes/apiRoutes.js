a0a57d7977e6b66eab706a68c462d6d56195e35a
EgRoku <jimenezjj5150@gmail.com>
2023-06-11 23:33:04 -0700

    added apiRoutes.js and htmlRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+// Depenency
+const path = require('path');
+const fs = require('fs');
+
+// npm package for unique id's
+const uniqeId = require('uniqeId');
+//const {application} = require('express');
+
+// route
+// Get /api/notes reads db.json and returns saved notes as Json
+module.exports = (application) => {
+    application.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
+    // Post /api/notes should receive a new json note to save on the request body, 
+    // and add it to the db.json file; then return the new note to the user.
+    application.post('/api/notes', (req, res) =>{
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        // creates a body and unique id for each note
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uniqeId(),
+        };
+        // push created note to db.json
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
+
+    // Delete /api/notes:id receives query param with uniqeId for a note to delete
+    application.delete('api/notes/:id', (req, res) => {
+        // Reads notes from db.json and removes them using uniqueId
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let deleteNote = db.filter(item => item.id !== req.params.id);
+        // Rewrites notes to db.json
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
+        res.json(deleteNote);
+    })
+};
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Dependancy
+//const { application } = require('express');
+const path = require('path');
+
+// Route
+module.exports = (application) => {
+    // Returns /notes.html
+    application.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '..public/notes.html'));
+    });
+    // Returns index.html
+    application.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    })
+};
